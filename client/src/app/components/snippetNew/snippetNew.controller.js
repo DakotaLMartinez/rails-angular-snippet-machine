17dc3c9fcdd1d1b2a629e138cf6c5964313cf29b
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-10-05 15:26:19 -0700

    adds create snippet functionality to SnippetNew page

--- client/src/app/components/snippetNew/snippetNew.controller.js
+++ client/src/app/components/snippetNew/snippetNew.controller.js
@@ -5,9 +5,15 @@
     .module('dlmSnippetMachine')
     .controller('SnippetNewController', SnippetNewController);
 
-  SnippetNewController.$inject = ['Snippet'];
-  function SnippetNewController(Snippet) {
+  SnippetNewController.$inject = ['Snippet', '$filter'];
+  function SnippetNewController(Snippet, $filter) {
     var vm = this;
+    var snippet = {}
+    vm.name = "";
+    vm.description = "";
+    vm.language = "";
+    vm.trigger = "";
+    vm.body = "";
     
 
     activate();
@@ -15,7 +21,21 @@
     ////////////////
 
     function activate() { 
-      
+      vm.submitForm = submitForm;
+      function submitForm(){
+        snippet.name = vm.name;
+        snippet.description = vm.description;
+        snippet.language = vm.language;
+        snippet.trigger = vm.trigger;
+        snippet.body = vm.body;
+        var data = $filter('json')(snippet);
+
+        Snippet
+          .createSnippet(data)
+          .then(function(res){
+            vm.response = res;
+          })
+      }
     }
   }
 })();
--- client/src/app/services/snippet.service.js
+++ client/src/app/services/snippet.service.js
@@ -9,19 +9,32 @@
   function Snippet($http) {
     this.getSnippets = getSnippets;
     this.getSnippet = getSnippet;
+    this.createSnippet = createSnippet;
 
     ////////////////
 
     function getSnippets() { 
-      return $http.get('/api/snippets').then(function(res){
-        return res.data;
-      });
+      return $http
+              .get('/api/snippets')
+              .then(function(res){
+                return res.data;
+              });
     }
 
     function getSnippet(id) {
-      return $http.get('/api/snippets/' + id).then(function(res){
-        return res.data;
-      });
+      return $http
+              .get('/api/snippets/' + id)
+              .then(function(res){
+                return res.data;
+              });
+    }
+
+    function createSnippet(data) {
+      return $http
+              .post('/api/snippets', data)
+              .then(function(res){
+                return res.data;
+              });
     }
 
   }
