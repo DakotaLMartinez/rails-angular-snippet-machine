5c0dfb44af4bd667bf9615d26d6b95fe91150736
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-10-12 15:07:33 -0700

    adds spacing to index.routes.js

--- client/src/app/index.route.js
+++ client/src/app/index.route.js
@@ -13,22 +13,30 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
-      }).state('register', {
+      })
+      
+      .state('register', {
         url: '/sign_up', 
         templateUrl: 'app/components/createAccount/createAccount.html', 
         controller: 'createAccountController', 
         controllerAs: 'vm'
-      }).state('signIn', {
+      })
+      
+      .state('signIn', {
         url: '/sign_in', 
         templateUrl: 'app/components/loginForm/loginForm.html', 
         controller: 'loginFormController', 
         controllerAs: 'vm'
-      }).state('snippetsIndex', {
+      })
+      
+      .state('snippetsIndex', {
         url: '/', 
         templateUrl: 'app/components/snippetIndex/snippetIndex.html', 
         controller: 'SnippetIndexController', 
         controllerAs: 'vm'
-      }).state('snippetNew', {
+      })
+      
+      .state('snippetNew', {
         url: '/snippets/new', 
         templateUrl: 'app/components/snippetForm/snippetForm.html',
         controller: 'SnippetNewController', 
@@ -43,17 +51,23 @@
             }
           }]
         }
-      }).state('snippetShow', {
+      })
+      
+      .state('snippetShow', {
         url: '/snippets/:id',
         templateUrl: 'app/components/snippetShow/snippetShow.html', 
         controller: 'SnippetShowController', 
         controllerAs: 'vm'
-      }).state('snippetShow.vscode', {
+      })
+      
+      .state('snippetShow.vscode', {
         url: '/vscode', 
         templateUrl: 'app/components/vsCodeView/vsCodeView.html',
         controller: 'SnippetShowController', 
         controllerAs: 'vm'
-      }).state('snippetEdit', {
+      })
+      
+      .state('snippetEdit', {
         url: '/snippets/:id/edit', 
         templateUrl: 'app/components/snippetForm/snippetForm.html', 
         controller: 'SnippetEditController', 
