63249d98f87cc6c0a56121e77d0da2ae828828a1
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-10-15 02:46:05 -0700

    shows delete button on show page only to users who can edit

--- client/src/app/components/snippetShow/snippetShow.controller.js
+++ client/src/app/components/snippetShow/snippetShow.controller.js
@@ -5,22 +5,41 @@
     .module('dlmSnippetMachine')
     .controller('SnippetShowController', SnippetShowController);
 
-  SnippetShowController.$inject = ['Snippet', '$stateParams'];
-  function SnippetShowController(Snippet, $stateParams) {
+  SnippetShowController.$inject = ['Snippet', '$stateParams', 'Session', '$state'];
+  function SnippetShowController(Snippet, $stateParams, Session, $state) {
     var vm = this;
     vm.snippet;
     vm.id = $stateParams.id;
+    vm.showDeleteButton;
+    vm.currentUser = Session.getCurrentUser();
 
     activate();
 
     ////////////////
 
     function activate() { 
-      Snippet
-        .getSnippet(vm.id)
-        .then(function(res){
+      getSnippet(vm.id);
+
+      function getSnippet(id) {
+        Snippet
+          .getSnippet(id)
+          .then(handleSuccess, handleError)
+          
+        function handleSuccess(res) {
           vm.snippet = res;
-        })
+          vm.errors = {};
+          if (vm.snippet.user_id === vm.currentUser.id) {
+            vm.showDeleteButton = true;
+          } else {
+            vm.showDeleteButton = false;
+          }
+        }
+      
+        function handleError(res){
+          vm.errors = res;
+          $state.go('snippetsIndex');
+        }
+      }
     }
   }
 })();
