febf31882217c3f4ef0ce50f3064da63d4e1061d
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-10-15 02:32:59 -0700

    adds session service to store user data in

--- client/src/app/services/session.service.js
+++ client/src/app/services/session.service.js
@@ -1,46 +1,35 @@
-// (function() {
-// 'use strict';
+(function() {
+'use strict';
 
-//   angular
-//     .module('dlmSnippetMachine')
-//     .service('session', SessionService);
+  angular
+    .module('dlmSnippetMachine')
+    .service('Session', Session);
 
-//   SessionService.$inject = ['$log', 'localStorage'];
-//   function SessionService($log, localStorage) {
-//     this._user = localStorage.getItem('session.user').fromJSON;
-//     this._accessToken = localStorage.getItem('session.accessToken').fromJSON;
-
-//     this.getUser = getUser;
-//     this.setUser = setUser;
-//     this.getAccessToken = getAccessToken;
-//     this.setAccessToken = setAccessToken;
-//     this.destroy = destroy;
+  Session.$inject = ['$cookies'];
+  function Session($cookies) {
+    this.getCurrentUser = getCurrentUser;
+    this.getCurrentUserId = getCurrentUserId;
+    this.setCurrentUser = setCurrentUser;
+    this.endSession = endSession;
     
-//     ////////////////
+    ////////////////
 
-//     function getUser() { 
-//       return this._user;
-//     }
+    function getCurrentUser() { 
+      return $cookies.getObject('currentUser');
+    }
 
-//     function setUser(user) {
-//       this._user = user;
-//       localStorage.setItem('session.user', user.toJSON);
-//       return this;
-//     }
+    function getCurrentUserId() {
+      return Number($cookies.get('currentUserId'));
+    }
 
-//     function getAccessToken() {
-//       return this._accessToken;
-//     }
+    function setCurrentUser(user) {
+      $cookies.putObject('currentUser', user);
+      $cookies.put('currentUserId', user.id);
+    }
 
-//     function setAccessToken(token) {
-//       this._accessToken = token; 
-//       localStorage.setItem('session.accessToken', token);
-//       return this;
-//     }
-
-//     function destroy() {
-//       this.setUser(null);
-//       this.setAccessToken(null);
-//     }
-//   }
-// })();
+    function endSession() {
+      $cookies.remove('currentUser');
+      $cookies.remove('currentUserId');
+    }
+  }
+})();
