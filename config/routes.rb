2c259dc250b941c2d07a34a47016bdee4fad9ce9
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-10-21 01:45:30 -0700

    removes vscode snippets controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: '/api/auth'
   scope '/api' do 
     resources :snippets
-    
-    get 'user_snippets/:user_id/language/:language', to: 'vscode_snippets#language'
-    resources :vscode_snippets, only: [:show]
 
     get 'dropbox/', to: 'dropbox#authorize'
     get 'dropbox/connect', to: 'dropbox#connect'
