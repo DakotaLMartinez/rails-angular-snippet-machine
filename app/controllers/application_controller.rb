9307831757be745b96ef0b376823ac242ce94fe8
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-12 13:15:26 -0800

    adds get_current_user method to replace non-functional current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::UrlFor
+  include ActionController::Cookies
 
   def set_redirect_url(url)
     if url.match(/localhost/)
@@ -17,6 +18,26 @@ class ApplicationController < ActionController::API
     else 
       "http://snippetmachine.dakotaleemartinez.com"
     end
+  end
+
+  def authenticate_current_user 
+    head :unauthorized if get_current_user.nil?
+  end
+
+  def get_current_user 
+    return nil unless cookies[:auth_headers]
+    auth_headers = JSON.parse(cookies[:auth_headers])
+
+    expiration_datetime = DateTime.strptime(auth_headers["expiry"], "%s")
+    current_user = User.find_by(uid: auth_headers["uid"])
+
+    if current_user &&
+       current_user.tokens.has_key?(auth_headers["client"]) &&
+       expiration_datetime > DateTime.now
+
+      @current_user = current_user
+    end
+    @current_user
   end
   
   # protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
