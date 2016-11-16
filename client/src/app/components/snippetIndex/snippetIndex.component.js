26330b6234e24fae5928928a80dd9bf4c746d874
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2016-11-16 14:31:46 -0800

    refactors snippetIndex component to new syntax + clarifies load/retry/error handling

--- client/src/app/components/snippetIndex/snippetIndex.component.js
+++ client/src/app/components/snippetIndex/snippetIndex.component.js
@@ -1,111 +1,122 @@
 (function() {
-  'use strict';
+'use strict';
+
   // Usage:
-  //
+  // 
   // Creates:
-  //
-
-  var SnippetIndex = {
-    bindings: {
-      userId: '=',
-      profilePage: '<'
-    },
-    templateUrl: 'app/components/snippetIndex/snippetIndex.html',
-    controller: SnippetIndexController,
-    controllerAs: 'vm'
-  };
-
-  SnippetIndexController.$inject = ['Snippet', 'User', 'Language', '$timeout', '$rootScope']
-  function SnippetIndexController (Snippet, User, Language, $timeout, $rootScope) {
-    var vm = this; 
-    vm.snippets = [];
-    vm.itemsPerPage = vm.itemsPerPage || 5;
-    vm.languages = Language.listSupportedLanguages();
-    if (vm.profilePage === true) {
-      vm.title = "My Snippets";
-    } else {
-      vm.title = "Snippets Index";
-    }
-    vm.showAddButton = User.loggedIn();
-    
-    activate();
-
-    ///////////////////////////////
-
-    $rootScope.$on('auth:logout-success', function(){
-      vm.showAddButton = User.loggedIn();
-    })
-
-    $rootScope.$on('auth:registration-email-success', function(){
-      vm.showAddButton = User.loggedIn();
-    })
-
-    $rootScope.$on('auth:login-success', function(){
-      vm.showAddButton = User.loggedIn();
-    })
-
-    function activate() {
-      vm.loadSnippets = loadSnippets;
-      vm.errors = {};
-      vm.showError = true;
-      vm.showAddButton = User.loggedIn();
-     
-      loadSnippets();
-
-      $timeout(loadSnippets, 1100);
-
-      function loadSnippets() {
-        if (vm.profilePage === true) {
-          if (vm.userId) {
-            getUserSnippets(vm.userId);
-          } else {
-            $timeout(function(){
-              if (vm.showError) {
-                vm.errors.user = ['must be loaded before snippets can be fetched - please click reload snippets button'];
-              }
-            }, 2500);
-          }
-        } else {
-          getAllSnippets();
-        }
-      }
-      
-      function getAllSnippets() {
-        Snippet
-          .getSnippets()
-          .then(handleSuccess, handleError)
-        
-        function handleSuccess(res) {
-          vm.snippets = res;
-          vm.errors = {};
-        }
-
-        function handleError(res){
-          vm.errors = res;
-        }
-      }
-
-      function getUserSnippets(id) {
-          User
-            .getUserSnippets(id)
-            .then(handleSuccess, handleError)
-          
-          function handleSuccess(res) {
-            vm.snippets = res;
-            vm.errors = {};
-            vm.showError = false;
-          }
-      
-          function handleError(res){
-            vm.errors = res;
-          }
-      }
-
-    }
-  }
+  // 
 
   angular
     .module('dlmSnippetMachine')
-    .component('snippetIndex', SnippetIndex);
+    .component('snippetIndex', {
+      templateUrl: 'app/components/snippetIndex/snippetIndex.html',
+      controller: SnippetIndexController,
+      bindings: {
+        userId: '=',
+        profilePage: '<'
+      },
+      controllerAs: 'vm'
+    });
 
+  SnippetIndexController.$inject = ['Snippet', 'User', 'Language', '$timeout', '$rootScope', '$log']
+  function SnippetIndexController (Snippet, User, Language, $timeout, $rootScope, $log) {
+    var $ctrl = this;
+    $ctrl.snippets = [];
+    $ctrl.itemsPerPage = $ctrl.itemsPerPage || 5;
+    $ctrl.languages = Language.listSupportedLanguages();
+    if ($ctrl.profilePage === true) {
+      $ctrl.title = "My Snippets";
+    } else {
+      $ctrl.title = "Snippets index";
+    }
+
+    $ctrl.loadSnippets = loadSnippets;
+    
+
+    ////////////////
+
+    $ctrl.$onInit = function() {
+      $ctrl.loadSnippets = loadSnippets; 
+      $ctrl.errors = {};
+      $ctrl.notLoaded = true;
+      $ctrl.showAddButton = User.loggedIn();
+
+      loadSnippets();
+
+      // adds retry to load snippets after 1.1 seconds 
+      // this prevents a premature error if user 
+      // not loaded before requesting snippets from api
+      // $timeout(loadSnippets,1100);
+
+      $rootScope.$on('auth:logout-success', function(){
+        loadSnippets(true);
+      })
+
+
+    };
+    $ctrl.$onChanges = function(changesObj) { };
+    $ctrl.$onDestroy = function() { };
+
+    ////////////////
+
+    function loadSnippets(reload) {
+      if ($ctrl.profilePage === true) {
+        if ($ctrl.userId) {
+          if ($ctrl.notLoaded || reload) {
+            getUserSnippets($ctrl.userId);
+          } else {
+            $log.log('$ctrl.notLoaded || reload is returning false');
+          }
+        } else {
+          // waits 2.5 seconds (till after retry of loadSnippets)
+          // before displaying an error. Displays an error if both 
+          // attempts to load snippets fail. (notLoaded is still true)
+          $timeout(function(){
+            if ($ctrl.notLoaded) {
+              $ctrl.errors.user = ['must be loaded before snippets can be fetched - please click reload snippets button'];
+            }
+          }, 2500);
+        }
+      } else {
+        if ($ctrl.notLoaded || reload) {
+          $log.log('should be calling api');
+          getAllSnippets();
+        } else {
+          $log.log('$ctrl.notLoaded || reload is returning false');
+        }
+      }
+    }
+
+    function getAllSnippets() {
+      Snippet 
+        .getSnippets()
+        .then(handleSuccess, handleError)
+
+      function handleSuccess(res) {
+        $ctrl.snippets = res; 
+        $ctrl.errors = {};
+        $ctrl.notLoaded = false;
+      }
+      
+      function handleError(res) {
+        $ctrl.errors = res;
+      }
+    }
+
+    function getUserSnippets(id) {
+      User 
+        .getUserSnippets(id)
+        .then(handleSuccess, handleError)
+
+      function handleSuccess(res) {
+        $ctrl.snippets = res; 
+        $ctrl.errors = {};
+        $ctrl.notLoaded = false;
+      }
+
+      function handleError(res) {
+        $ctrl.errors = res;
+      }
+    }
+  }
 })();
